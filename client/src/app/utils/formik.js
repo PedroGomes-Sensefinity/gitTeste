9a0be5e8c791524233abfa4cf862f95b1034f013
Alexandre Di Salvo <alexandre.disalvo@sensefinity.com>
2021-06-16 09:53:33 -0300

    IC-1214-Device-provision

---
+++ client/src/app/utils/formik.js
@@ -0,0 +1,9 @@
+export const getInputClasses = (formik, fieldname) => {
+    if (formik.touched[fieldname] && formik.errors[fieldname]) {
+        return 'is-invalid';
+    }
+    if (formik.touched[fieldname] && !formik.errors[fieldname]) {
+        return 'is-valid';
+    }
+    return '';
+};
