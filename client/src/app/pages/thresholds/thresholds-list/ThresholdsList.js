f52365ed522c39d2ff649cc08085a1a88d2a9c7a
Fabiano Costa <fabiano.costa@sensefinity.com>
2021-07-13 06:33:34 -0300

    IC-1250-Threshold-list

--- client/src/app/pages/thresholds/thresholds-list/ThresholdsList.js
+++ client/src/app/pages/thresholds/thresholds-list/ThresholdsList.js
@@ -1,5 +1,67 @@
-import React from "react";
-                  
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import history from '../../../history';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardHeader, CardContent, Button } from '@material-ui/core';
+
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import TableGrid from '../../../components/table-grid/table-grid.component';
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+        margin: theme.spacing(1),
+    },
+    rightIcon: {
+        marginLeft: theme.spacing(1),
+    },
+    leftIcon: {
+        marginRight: theme.spacing(1),
+    },
+    iconSmall: {
+        fontSize: 20,
+    },
+}));
+
 export function ThresholdsList() {
-    return <h1>ThresholdsList</h1>
-}  
+    const classes = useStyles();
+
+    const columns = [
+        {
+            field: 'name',
+            title: 'Name',
+        }
+    ];
+
+    return (
+        <Card>
+            <CardContent>
+                <Link to='/thresholds/new'>
+                    <Button
+                        variant='contained'
+                        color='secondary'
+                        className={classes.button}>
+                        <AddIcon className={classes.leftIcon} />
+                        New threshold
+                    </Button>
+                </Link>
+                <TableGrid
+                    actions={[
+                        {
+                            icon: EditIcon,
+                            tooltip: 'Edit threshold',
+                            onClick: (event, rowData) => {
+                                history.push(`/thresholds/edit/${rowData.id}`);
+                            },
+                        },
+                    ]}
+                    title=''
+                    columns={columns}
+                    endpoint={'threshold'}
+                    dataField='thresholds'
+                />
+            </CardContent>
+        </Card>
+    );
+}
