4e605cdfaee214f4266928b4c3034b38a2f54081
Alexandre Di Salvo <alexandre.disalvo@sensefinity.com>
2021-09-15 08:41:20 -0300

    Review groups

--- client/src/app/services/groupsService.js
+++ client/src/app/services/groupsService.js
@@ -5,6 +5,40 @@ const groupsService = {
         return axios.get(`${process.env.REACT_APP_REST_API_URL}group/limit/${limit}/offset/${offset}`)
                     .then(async r => r.data.data);
     },
+    save: function (formData) {
+        return new Promise(function (resolve, reject) {
+            axios.post(`${process.env.REACT_APP_REST_API_URL}group/`, formData)
+                .then((response) => response.data)
+                .then((responseData) => {
+                    let result = [];
+                    if (responseData.code === 200) {
+                        result = responseData.data;
+                    }
+                    resolve(result);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                    reject(Error("Something went wrong on create group... "));
+                });
+        });
+    },
+    update: function (formData) {
+        return new Promise(function (resolve, reject) {
+            axios.put(`${process.env.REACT_APP_REST_API_URL}group/${formData.id}`, formData)
+                .then((response) => response.data)
+                .then((responseData) => {
+                    let result = [];
+                    if (responseData.code === 200) {
+                        result = responseData.data;
+                    }
+                    resolve(result);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                    reject(Error("Something went wrong on update group... "));
+                });
+        });
+    }
 }
 
 export default groupsService;
