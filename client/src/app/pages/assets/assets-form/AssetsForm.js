63be2022b17205f68739c53554ffcde6fc0632c2
Fabiano Costa <fabiano.costa@sensefinity.com>
2021-12-21 13:46:37 +0000

    Closing IC-1419

--- client/src/app/pages/assets/assets-form/AssetsForm.js
+++ client/src/app/pages/assets/assets-form/AssetsForm.js
@@ -1,5 +1,32 @@
 import React from "react";
+import {Paper, Tab, Tabs} from "@material-ui/core";
+import {TabContainer} from "react-bootstrap";
 
-export function AssetsForm() {
-    return <h1>AlarmsForm</h1>
+import AssetsFormComponent from "../../../components/form/AssetsFormComponent";
+import AssetDevicesComponent from "../../../components/form/AssetDevicesComponent";
+
+export function AssetsForm({match}) {
+    const {id} = match.params;
+    const [value, setValue] = React.useState(0);
+
+    function handleChange(event, newValue) {
+        setValue(newValue);
+    }
+
+    return (<div>
+        <Paper square>
+            <Tabs value={value} indicatorColor="primary" textColor="primary" onChange={handleChange}>
+                <Tab label="Asset"/>
+                <Tab label="Devices" disabled={typeof id === 'undefined'} />
+            </Tabs>
+        </Paper>
+        <TabContainer>
+            {value === 0 && <AssetsFormComponent id={id} />}
+        </TabContainer>
+        <TabContainer>
+            {value === 1 && <AssetDevicesComponent id={id} />}
+        </TabContainer>
+    </div>
+
+    );
 }
