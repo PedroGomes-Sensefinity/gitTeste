f93fea7c31d9260399b7b473cf4c778d0d9e86bc
Tarcísio de Paula <tarcisio.paula@sensefinity.com>
2022-05-02 10:57:50 +0000

    Develop

--- client/src/app/pages/thresholds/thresholds-list/ThresholdsList.js
+++ client/src/app/pages/thresholds/thresholds-list/ThresholdsList.js
@@ -1,9 +1,9 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import history from '../../../history';
 import { makeStyles } from '@material-ui/core/styles';
 import { Card, CardContent, Button } from '@material-ui/core';
+import apiService from '../../../services/apiService'
 
 import AddIcon from '@material-ui/icons/Add';
 import EditIcon from '@material-ui/icons/Edit';
@@ -25,14 +25,65 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export function ThresholdsList() {
+    const [data, setData] = React.useState([]);
     const classes = useStyles();
 
     const columns = [
         {
             field: 'name',
             title: 'Name',
+        },
+        {
+            field: 'type',
+            title: 'Type',
         }
     ];
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = () => {
+        new Promise((resolve, reject) => {
+            let method = 'get';
+            let params = ['threshold', 999999, 0];
+
+            apiService[method](...params)
+            .then((result) => {
+                resolve({
+                    data: result['thresholds'],
+                    page: 0,
+                    totalCount: result.total,
+                });
+            });
+        })
+        .then((result) => {
+            console.log(result);
+            result.data.forEach(threshold => {
+                const rule = JSON.parse(threshold['rule']);
+                switch(rule['type']) {
+                    case 'temperaturedegree':
+                    case 'temperature':
+                        threshold['type'] = "Temperature";
+                        break;
+                    case 'geofences':
+                    case 'geofence':
+                        threshold['type'] = "Geo-fences";
+                        break;
+                    case 'humidityrelative':
+                        threshold['type'] = "Humidity";
+                        break;
+                    case 'buttonpressed':
+                        threshold['type'] = "Button pressed"
+                        break;
+                    default:
+                        console.log("Error: Unidentified threshold type.")
+                }
+            })
+            setData(result.data);
+        });
+    }
+
 
     return (
         <Card>
@@ -58,8 +109,7 @@ export function ThresholdsList() {
                     ]}
                     title=''
                     columns={columns}
-                    endpoint={'threshold'}
-                    dataField='thresholds'
+                    data = {data}
                 />
             </CardContent>
         </Card>
