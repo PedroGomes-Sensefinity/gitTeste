af2088ed39d6e0d8c9cb04444399d2e7b8c57c80
Daniel Dias <daniel.dias@sensefinity.com>
2022-10-03 11:11:45 +0100

    fixes device selector being disaligned with text by removing adjacent text and introducing a label to Select component

--- client/src/app/pages/assets/assets-form/DeviceSelector.jsx
+++ client/src/app/pages/assets/assets-form/DeviceSelector.jsx
@@ -1,18 +1,32 @@
-import { Card, CardContent, CircularProgress, MenuItem, Select } from '@material-ui/core';
+import { Card, CardContent, MenuItem, Select, InputLabel, FormControl} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import React, { useEffect, useState } from 'react';
 import { useHistory } from "react-router-dom";
 import DeviceDashboard from "../../../components/form/DeviceDashboard";
 import apiService from '../../../services/apiService';
+import BlockUi from "react-block-ui";
 
-
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
 
 export default function DeviceSelector({assetId}) {
     const [devices, setDevices] = useState([])
     const [deviceId, setDeviceId] = useState("")
     const [isLoading, setLoading] = useState(true)
+
+    const classes = useStyles()
+    // we use this hook so it doesn't show the splash screen
     const history = useHistory()
 
     useEffect(() => {
+        setLoading(true)
         apiService.getById(`asset/`, assetId ).then((results) => {
             const asset = results.assets[0]
             const devices = asset.devices || []
@@ -31,25 +45,22 @@ export default function DeviceSelector({assetId}) {
         setDeviceId(newId)
     }
 
-    if (isLoading)
-            return <CircularProgress />
-    else {
-       return  <>
+    // add transition to smooth things up, currently change just flashes
+    return <BlockUi tag='div' blocking={isLoading}>
             <Card>
                 <CardContent>
-                    <h3 className='card-label font-weight-bolder text-dark'>Select Device:  
-                    <Select autoWidth={true} onChange={handleOnChange} value={deviceId}>
-                        {[<MenuItem key="asdsada" value=""></MenuItem>].concat(devices.map(device => 
-                            <MenuItem key={device.id} value={device.id}>
-                                <h3 className='card-label font-weight-bolder text-dark'>{device.id}</h3>
-                            </MenuItem>)
-                            )
-                            }
-                    </Select>
-                    </h3>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id='device-id-input-label'>Select Device</InputLabel>
+                        <Select labelId='device-id-input-label' value={deviceId} onChange={handleOnChange} displayEmtpy className={classes.selectEmpty} >
+                            {devices.map(device => 
+                                <MenuItem key={device.id} value={device.id}>
+                                    <h3 className='card-label font-weight-bolder text-dark'>{device.id}</h3>
+                                </MenuItem>)
+                                }
+                        </Select>
+                    </FormControl>
                 </CardContent>
             </Card>
             {deviceId !== "" ? <DeviceDashboard key={deviceId} id={deviceId}/>: <></>}
-       </>
-    }
+            </BlockUi>
 }
