004561483bd673bd2e996376ade6300f91046c61
Daniel Dias <daniel.dias@sensefinity.com>
2022-10-06 09:12:48 +0100

    cleans up unnused imports

--- client/src/app/pages/assets/assets-form/DeviceSelector.jsx
+++ client/src/app/pages/assets/assets-form/DeviceSelector.jsx
@@ -1,29 +1,10 @@
-<<<<<<< Updated upstream
-import { Card, CardContent, MenuItem, Select, InputLabel, FormControl} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardContent, MenuItem, Select } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
-=======
-import { Card, CardContent, CircularProgress, MenuItem, Select } from '@material-ui/core';
-import React, { Fragment, useEffect, useState } from 'react';
->>>>>>> Stashed changes
+import BlockUi from "react-block-ui";
 import { useHistory } from "react-router-dom";
 import DeviceDashboard from "../../../components/form/DeviceDashboard";
 import apiService from '../../../services/apiService';
-import BlockUi from "react-block-ui";
 
-<<<<<<< Updated upstream
-const useStyles = makeStyles((theme) => ({
-    formControl: {
-      margin: theme.spacing(1),
-      minWidth: 120,
-    },
-    selectEmpty: {
-      marginTop: theme.spacing(2),
-    },
-  }));
-
-=======
->>>>>>> Stashed changes
 export default function DeviceSelector({assetId}) {
     const [devices, setDevices] = useState([])
     const [deviceId, setDeviceId] = useState("")
@@ -57,18 +38,6 @@ export default function DeviceSelector({assetId}) {
     return <BlockUi tag='div' blocking={isLoading}>
             <Card>
                 <CardContent>
-<<<<<<< Updated upstream
-                    <FormControl className={classes.formControl}>
-                        <InputLabel id='device-id-input-label'>Select Device</InputLabel>
-                        <Select labelId='device-id-input-label' value={deviceId} onChange={handleOnChange} displayEmtpy className={classes.selectEmpty} >
-                            {devices.map(device => 
-                                <MenuItem key={device.id} value={device.id}>
-                                    <h3 className='card-label font-weight-bolder text-dark'>{device.id}</h3>
-                                </MenuItem>)
-                                }
-                        </Select>
-                    </FormControl>
-=======
                     <div>
                         <h3 className='card-label font-weight-bolder text-dark d-inline-block'>Select Device:</h3>
                         <div className='d-inline-block pl-5'>
@@ -81,7 +50,6 @@ export default function DeviceSelector({assetId}) {
                             </Select>
                         </div>
                     </div>
->>>>>>> Stashed changes
                 </CardContent>
             </Card>
             {deviceId !== "" ? <DeviceDashboard key={deviceId} id={deviceId}/>: <></>}
