0fc538eac6653a3cc31efa11451b791d201ef95e
Martim Mourao <martim.mourao@sensefinity.com>
2023-01-10 13:53:26 +0000

    Testing new size

--- client/src/app/pages/impacts/Impacts.js
+++ client/src/app/pages/impacts/Impacts.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import * as THREE from "three";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
-import Container from './Container'
-import { useLoader } from '@react-three/fiber'
+import { Suspense } from 'react'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 function Line(x, y, z, color) {
   const material = new THREE.LineBasicMaterial({ color: color, linewidth: 50 });
   const points = [];
-  points.push(new THREE.Vector3(0, 0, 0));
-  points.push(new THREE.Vector3(x, y, z));
+  points.push(new THREE.Vector3(0, 1, 0));
+  points.push(new THREE.Vector3(x, y +1, z));
   const geometry = new THREE.BufferGeometry().setFromPoints(points);
   const line = new THREE.Line(geometry, material);
   return line
@@ -31,33 +33,57 @@ export function Impacts() {
     { id: 6, label: "Container Down" },
   ]
   const [selectedOption, setSelectedOption] = useState(0);
-  const [selectedContainer, setSelectedContainer] = useState(<Container file={"/media/3d/container/F/containerF.gltf"}></Container>)
+  const [selectedContainer, setSelectedContainer] = useState("/media/3d/container/R/containerR.gltf")
 
   function onChangeOption(e) {
     setSelectedOption(e.target.value);
     console.log(e.target.value)
     switch (e.target.value) {
       case 1:
-        setSelectedContainer(<Container file={"/media/3d/container/F/containerF.gltf"}></Container>)
+        setSelectedContainer("/media/3d/container/R/containerR.gltf")
         break
       case 2:
-        setSelectedContainer(<Container file={"/media/3d/container/B/containerB.gltf"}></Container>)
+        setSelectedContainer("/media/3d/container/R/containerR.gltf")
         break
       case 3:
-        setSelectedContainer(<Container file={"/media/3d/container/L/containerL.gltf"}></Container>)
+        setSelectedContainer("/media/3d/container/R/containerR.gltf")
         break
       case 4:
-        setSelectedContainer(<Container file={"/media/3d/container/R/containerR.gltf"}></Container>)
+        setSelectedContainer("/media/3d/container/R/containerR.gltf")
         break
       case 5:
-        setSelectedContainer(<Container file={"/media/3d/container/T/containerT.gltf"}></Container>)
+        setSelectedContainer("/media/3d/container/R/containerR.gltf")
         break
       case 6:
-        setSelectedContainer(<Container file={"/media/3d/container/D/containerD.gltf"}></Container>)
+        setSelectedContainer("/media/3d/container/R/containerR.gltf")
         break
     }
 
   }
+  const scene = new THREE.Scene();
+  const loader = new GLTFLoader();
+
+  const draco = new DRACOLoader();
+  draco.setDecoderConfig({ type: 'js' });
+  draco.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
+  loader.setDRACOLoader(draco);
+
+  // Load a glTF resource
+  loader.load(
+    selectedContainer,
+    function (gltf) {
+      scene.add(gltf.scene);
+      scene.translateY(1)
+    },
+    // loading is progressing
+    function (xhr) {
+      console.log("Loading...");
+    },
+    // error
+    function (error) {
+      console.log('An error happened');
+    }
+  );
 
   return (
     <div className='row'>
@@ -72,7 +98,18 @@ export function Impacts() {
         </FormControl>
       </div>
       <div key={selectedContainer} className='col-xl-11 ' style={{ height: '100vh' }}>
-        {selectedContainer}
+        <Canvas camera={{ fov: 50 }} key={selectedContainer} >
+          <ambientLight intensity={1} />
+          <spotLight position={[20, 20, 20]} penumbra={1} />
+          <pointLight position={[20, 20, 20]} />
+          <spotLight position={[-20, -20, -20]} penumbra={1} />
+          <pointLight position={[-20, -20, -20]} />
+          <primitive object={Line(2, 1, 1, "#DD1C1C")} />
+          <Suspense fallback={null}>
+            <primitive object={scene} />
+          </Suspense>
+          <OrbitControls/>
+        </Canvas>
       </div>
     </div>
 
